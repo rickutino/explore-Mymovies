2d70b49ea2fa6c92ccc599098c557db6ae30b52a
rickutino <rickutino@gmail.com>
2022-08-31 22:52:08 +0900

    add theme colors

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    RED_800: "#FF859B0D",
+    RED_200: "#FF859B",
+
+    BLACK_900: "#1C1B1E",
+    BLACK_800: "#262529",
+    BLACK_500: "#312E38",
+    BLACK_200: "#3E3B47",
+
+    GRAY_700: "#948F99",
+    GRAY_200: "#CAC4CF",
+    GRAY_100: "#F4EDE8",
+    WHITE: "#FFFFFF",
+  }
+}
